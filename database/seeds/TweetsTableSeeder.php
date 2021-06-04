078ee3dcf55971fc23f2a335a1868de653180c8b
Takuya Kogawa <tk.ngskgtc.vt@gmail.com>
2021-06-04 19:50:11 +0900

    update

--- database/seeds/TweetsTableSeeder.php
+++ database/seeds/TweetsTableSeeder.php
@@ -12,9 +12,11 @@ class TweetsTableSeeder extends Seeder
      */
     public function run()
     {
-        for ($i = 1; $i <= 10; $i++) {
+        // for ($i = 1; $i <= 10; $i++) {
+        for ($i = 0; $i < 10; $i++) {
             Tweet::create([
-                'user_id'    => $i,
+                // 'user_id'    => $i,
+                'user_id'    => $i * 10 + 5,
                 'text'       => 'テスト投稿' . $i,
                 'created_at' => now(),
                 'updated_at' => now()
