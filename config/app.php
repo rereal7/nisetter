39b10bb7a5984590e9285a1c62034946a91c0f22
Takuya Kogawa <tk.ngskgtc.vt@gmail.com>
2021-06-04 17:54:07 +0900

    title rename

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Twitter_Clone'),
+    'name' => env('APP_NAME', 'Nisetter'),
 
     /*
     |--------------------------------------------------------------------------
